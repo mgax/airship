7b1832e742d07655351bc055ee879ad7d74c6e88
Alex Morega <alex@grep.ro>
2012-06-10 23:29:09 +0300

    unit test for vagrant deployment, the hard way

--- vagrant/deployment_test.py
+++ vagrant/deployment_test.py
@@ -38,6 +38,10 @@ def remote_listdir(name):
     return json.loads(run(cmd))
 
 
+def put_json(data, remote_path, **kwargs):
+    return put(StringIO(json.dumps(data)), str(remote_path), **kwargs)
+
+
 class VagrantDeploymentTest(unittest.TestCase):
 
     def setUp(self):
@@ -52,9 +56,7 @@ class VagrantDeploymentTest(unittest.TestCase):
             sudo("rm -rf '%s'" % (cfg['sarge-home']/name,))
 
     def configure(self, config):
-        put(StringIO(json.dumps(config)),
-            str(cfg['sarge-home']/sarge.DEPLOYMENT_CFG),
-            use_sudo=True)
+        put_json(config, cfg['sarge-home']/sarge.DEPLOYMENT_CFG, use_sudo=True)
 
     def test_ping(self):
         self.configure({'deployments': []})
@@ -63,3 +65,51 @@ class VagrantDeploymentTest(unittest.TestCase):
         sudo("'%(sarge-venv)s'/bin/supervisord "
              "-c '%(sarge-home)s'/supervisord.conf" % cfg)
         assert run('pwd') == '/home/vagrant'
+
+    def test_deploy_simple_wsgi_app(self):
+        self.configure({
+            'plugins': ['sarge:NginxPlugin'],
+            'deployments': [{'name': 'testy'}],
+        })
+        sarge_cmd = ("'%(sarge-venv)s'/bin/python "
+                     "/sarge-src/sarge.py '%(sarge-home)s' " % cfg)
+        supervisorctl_cmd = ("'%(sarge-venv)s'/bin/supervisorctl "
+                             "-c '%(sarge-home)s'/supervisord.conf " % cfg)
+        sudo(sarge_cmd + "init")
+        sudo("'%(sarge-venv)s'/bin/supervisord "
+             "-c '%(sarge-home)s'/supervisord.conf" % cfg)
+
+        version_folder = path(sudo(sarge_cmd + "new_version testy"))
+
+        nginx_symlink = '/etc/nginx/sites-enabled/testy'
+        sudo("ln -s '%s' '%s'"
+             % (version_folder/'nginx-site.conf', nginx_symlink))
+        self.addCleanup(sudo, "rm %s" % nginx_symlink)
+
+        url_cfg = {
+            'type': 'wsgi',
+            'url': '/',
+            'wsgi_app': 'mytinyapp:theapp',
+        }
+        put_json({'urlmap': [url_cfg], 'nginx_options': {'listen': '8013'}},
+                 version_folder/'sargeapp.yaml', use_sudo=True)
+        app_py = ('def theapp(environ, start_response):\n'
+                  '    start_response("200 OK", [])\n'
+                  '    return ["hello sarge!\\n"]\n')
+        put(StringIO(app_py), str(version_folder/'mytinyapp.py'), use_sudo=True)
+        sudo(sarge_cmd + "activate_version testy '%s'" % version_folder)
+        sudo(supervisorctl_cmd + "reread")
+        sudo(supervisorctl_cmd + "add testy")
+
+        # force a stop/start because start waits for program to be up (2s)
+        sudo(supervisorctl_cmd + "stop testy")
+        sudo(supervisorctl_cmd + "start testy")
+
+        try:
+            import urllib
+            f = urllib.urlopen('http://192.168.13.13:8013/')
+            data = f.read()
+            self.assertEqual(data, "hello sarge!\n")
+
+        finally:
+            sudo(supervisorctl_cmd + "shutdown")
