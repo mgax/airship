9b386978cae06df4d1830bf83aeba74886d599a7
Alex Morega <alex@grep.ro>
2012-07-27 21:39:01 +0300

    clean up whitespace in vagrant tests

--- vagrant/deployment_test.py
+++ vagrant/deployment_test.py
@@ -19,13 +19,13 @@ def provision():
 
 def setUpModule(self):
     import sarge, sarge.core; self.sarge = sarge
-    env['key_filename'] = path(__file__).parent/'vagrant_id_rsa'
+    env['key_filename'] = path(__file__).parent / 'vagrant_id_rsa'
     env['host_string'] = 'vagrant@192.168.13.13'
     if not exists(cfg['sarge-venv']):
         provision()
 
     self._nginx_symlink = '/etc/nginx/sites-enabled/testy'
-    nginx_all_sites = cfg['sarge-home']/'nginx.plugin'/'sarge_sites.conf'
+    nginx_all_sites = cfg['sarge-home'] / 'nginx.plugin' / 'sarge_sites.conf'
     sudo("ln -s '%s' '%s'" % (nginx_all_sites, self._nginx_symlink))
 
 
@@ -38,6 +38,7 @@ def tearDownModule(self):
 def sarge_cmd(cmd):
     base = ("'%(sarge-venv)s'/bin/sarge '%(sarge-home)s' " % cfg)
     return sudo(base + cmd)
+
 
 def supervisorctl_cmd(cmd):
     base = ("'%(sarge-venv)s'/bin/supervisorctl "
@@ -68,7 +69,7 @@ class VagrantDeploymentTest(unittest.TestCase):
     def setUp(self):
         sudo("mkdir '%(sarge-home)s'" % cfg)
         put_json({'plugins': ['sarge:NginxPlugin']},
-                 cfg['sarge-home']/sarge.core.SARGE_CFG,
+                 cfg['sarge-home'] / sarge.core.SARGE_CFG,
                  use_sudo=True)
         sarge_cmd("init")
         sudo("'%(sarge-venv)s'/bin/supervisord "
@@ -85,7 +86,9 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'name': 'testy',
                   'user': 'vagrant',
                   'nginx_options': {'listen': '8013'}},
-                 cfg['sarge-home']/sarge.core.DEPLOYMENT_CFG_DIR/'testy.yaml',
+                 (cfg['sarge-home'] /
+                    sarge.core.DEPLOYMENT_CFG_DIR /
+                    'testy.yaml'),
                  use_sudo=True)
 
         version_folder = path(sarge_cmd("new_version testy"))
@@ -95,13 +98,13 @@ class VagrantDeploymentTest(unittest.TestCase):
                      'url': '/',
                      'app_factory': 'mytinyapp:gettheapp'},
                  ]},
-                 version_folder/'sargeapp.yaml')
+                 version_folder / 'sargeapp.yaml')
         app_py = ('def gettheapp(appcfg):\n'
                   '    def theapp(environ, start_response):\n'
                   '        start_response("200 OK", [])\n'
                   '        return ["hello sarge!\\n"]\n'
                   '    return theapp\n')
-        put(StringIO(app_py), str(version_folder/'mytinyapp.py'))
+        put(StringIO(app_py), str(version_folder / 'mytinyapp.py'))
         sarge_cmd("activate_version testy '%s'" % version_folder)
 
         self.assertEqual(get_url('http://192.168.13.13:8013/'),
@@ -124,14 +127,16 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'name': 'testy',
                   'user': 'vagrant',
                   'nginx_options': {'listen': '8013'}},
-                 cfg['sarge-home']/sarge.core.DEPLOYMENT_CFG_DIR/'testy.yaml',
+                 (cfg['sarge-home'] /
+                    sarge.core.DEPLOYMENT_CFG_DIR /
+                    'testy.yaml'),
                  use_sudo=True)
 
         # deploy version one
         version_folder_1 = path(sarge_cmd("new_version testy"))
-        put_json(app_cfg, version_folder_1/'sargeapp.yaml')
+        put_json(app_cfg, version_folder_1 / 'sargeapp.yaml')
         put(StringIO(app_py_tmpl % 'one'),
-            str(version_folder_1/'mytinyapp.py'))
+            str(version_folder_1 / 'mytinyapp.py'))
         sarge_cmd("activate_version testy '%s'" % version_folder_1)
 
         self.assertEqual(get_url('http://192.168.13.13:8013/'),
@@ -139,9 +144,9 @@ class VagrantDeploymentTest(unittest.TestCase):
 
         # deploy version two
         version_folder_2 = path(sarge_cmd("new_version testy"))
-        put_json(app_cfg, version_folder_2/'sargeapp.yaml')
+        put_json(app_cfg, version_folder_2 / 'sargeapp.yaml')
         put(StringIO(app_py_tmpl % 'two'),
-            str(version_folder_2/'mytinyapp.py'))
+            str(version_folder_2 / 'mytinyapp.py'))
         sarge_cmd("activate_version testy '%s'" % version_folder_2)
 
         self.assertEqual(get_url('http://192.168.13.13:8013/'),
@@ -151,7 +156,9 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'name': 'testy',
                   'user': 'vagrant',
                   'nginx_options': {'listen': '8013'}},
-                 cfg['sarge-home']/sarge.core.DEPLOYMENT_CFG_DIR/'testy.yaml',
+                 (cfg['sarge-home'] /
+                    sarge.core.DEPLOYMENT_CFG_DIR /
+                    'testy.yaml'),
                  use_sudo=True)
 
         version_folder = path(sarge_cmd("new_version testy"))
@@ -159,10 +166,10 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'urlmap': [
                     {'type': 'php', 'url': '/'},
                  ]},
-                 version_folder/'sargeapp.yaml')
+                 version_folder / 'sargeapp.yaml')
 
         app_php = ('<?php echo "hello from" . " PHP!\\n"; ?>')
-        put(StringIO(app_php), str(version_folder/'someapp.php'))
+        put(StringIO(app_php), str(version_folder / 'someapp.php'))
         sarge_cmd("activate_version testy '%s'" % version_folder)
 
         self.assertEqual(get_url('http://192.168.13.13:8013/someapp.php'),
@@ -172,7 +179,9 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'name': 'testy',
                   'user': 'vagrant',
                   'nginx_options': {'listen': '8013'}},
-                 cfg['sarge-home']/sarge.core.DEPLOYMENT_CFG_DIR/'testy.yaml',
+                 (cfg['sarge-home'] /
+                    sarge.core.DEPLOYMENT_CFG_DIR /
+                    'testy.yaml'),
                  use_sudo=True)
 
         version_folder = path(sarge_cmd("new_version testy"))
@@ -180,7 +189,7 @@ class VagrantDeploymentTest(unittest.TestCase):
         put_json({'urlmap': [
                     {'type': 'static', 'url': '/', 'path': ''},
                  ]},
-                 version_folder/'sargeapp.yaml')
+                 version_folder / 'sargeapp.yaml')
 
         with cd(str(version_folder)):
             run("echo 'hello static!' > hello.html")
