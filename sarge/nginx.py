a22b75429d47acfa97f11894c4922cf2550e1b0e
Alex Morega <alex@grep.ro>
2012-07-27 21:35:08 +0300

    reorganize nginx configuration generator

--- sarge/nginx.py
+++ sarge/nginx.py
@@ -13,6 +13,37 @@ class NginxPlugin(object):
     log = logging.getLogger('sarge.NginxPlugin')
     log.setLevel(logging.DEBUG)
 
+    WSGI_TEMPLATE = (
+        'location %(url)s {\n'
+        '    include %(fcgi_params_path)s;\n'
+        '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
+        '    fastcgi_param SCRIPT_NAME "";\n'
+        '    fastcgi_pass unix:%(socket_path)s;\n'
+        '}\n')
+
+    STATIC_TEMPLATE = (
+        'location %(url)s {\n'
+        '    alias %(version_folder)s/%(path)s;\n'
+        '}\n')
+
+    PHP_TEMPLATE = (
+        'location %(url)s {\n'
+        '    include %(fcgi_params_path)s;\n'
+        '    fastcgi_param SCRIPT_FILENAME '
+                '%(version_folder)s$fastcgi_script_name;\n'
+        '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
+        '    fastcgi_param SCRIPT_NAME "";\n'
+        '    fastcgi_pass unix:%(socket_path)s;\n'
+        '}\n')
+
+    FCGI_TEMPLATE = (
+        'location %(url)s {\n'
+        '    include %(fcgi_params_path)s;\n'
+        '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
+        '    fastcgi_param SCRIPT_NAME "";\n'
+        '    fastcgi_pass %(socket)s;\n'
+        '}\n')
+
     def __init__(self, sarge):
         self.sarge = sarge
         sarge.on_activate_version.connect(self.activate_deployment, weak=False)
@@ -57,74 +88,60 @@ class NginxPlugin(object):
         self.log.debug("Writing nginx configuration for deployment %r at %r.",
                        depl.name, conf_path)
 
+        conf_options = ""
+        nginx_options = depl.config.get('nginx_options', {})
+        for key, value in sorted(nginx_options.items()):
+            conf_options += '  %s %s;\n' % (key, value)
+
+        conf_urlmap = ""
+
+        for entry in app_config.get('urlmap', []):
+            self.log.debug("urlmap entry: %r", entry)
+
+            if entry['type'] == 'static':
+                conf_urlmap += self.STATIC_TEMPLATE % dict(entry,
+                        version_folder=version_folder)
+            elif entry['type'] == 'wsgi':
+                socket_path = run_folder / 'wsgi-app.sock'
+                conf_urlmap += self.WSGI_TEMPLATE % dict(entry,
+                        socket_path=socket_path,
+                        fcgi_params_path=self.fcgi_params_path)
+                depl.config['tmp-wsgi-app'] = entry['app_factory']
+
+            elif entry['type'] == 'php':
+                socket_path = run_folder / 'php.sock'
+                conf_urlmap += self.PHP_TEMPLATE % dict(entry,
+                        socket_path=socket_path,
+                        version_folder=version_folder,
+                        fcgi_params_path=self.fcgi_params_path)
+
+                share['programs'].append({
+                    'name': 'fcgi_php',
+                    'command': (
+                        '/usr/bin/spawn-fcgi -s %(socket_path)s -M 0777 '
+                        '-f /usr/bin/php5-cgi -n'
+                        % {'socket_path': socket_path})
+                })
+
+            elif entry['type'] == 'fcgi':
+                socket_uri = entry['socket']
+                if socket_uri.startswith('tcp://'):
+                    socket = socket_uri[len('tcp://'):]
+                elif socket_uri.startswith('unix:///'):
+                    socket = 'unix:' + socket_uri[len('unix://'):]
+                else:
+                    raise ValueError("Can't parse socket %r" % socket_uri)
+                conf_urlmap += self.FCGI_TEMPLATE % dict(entry,
+                        socket=socket,
+                        fcgi_params_path=self.fcgi_params_path)
+
+            else:
+                raise NotImplementedError
+
         with open(conf_path, 'wb') as f:
             f.write('server {\n')
-
-            nginx_options = depl.config.get('nginx_options', {})
-            for key, value in sorted(nginx_options.items()):
-                f.write('  %s %s;\n' % (key, value))
-
-            for entry in app_config.get('urlmap', []):
-                self.log.debug("urlmap entry: %r", entry)
-
-                if entry['type'] == 'static':
-                    f.write('location %(url)s {\n'
-                            '    alias %(version_folder)s/%(path)s;\n'
-                            '}\n' % dict(entry, version_folder=version_folder))
-                elif entry['type'] == 'wsgi':
-                    socket_path = run_folder / 'wsgi-app.sock'
-                    f.write('location %(url)s {\n'
-                            '    include %(fcgi_params_path)s;\n'
-                            '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
-                            '    fastcgi_param SCRIPT_NAME "";\n'
-                            '    fastcgi_pass unix:%(socket_path)s;\n'
-                            '}\n' % dict(entry,
-                                         socket_path=socket_path,
-                                         fcgi_params_path=self.fcgi_params_path))
-                    depl.config['tmp-wsgi-app'] = entry['app_factory']
-
-                elif entry['type'] == 'php':
-                    socket_path = run_folder / 'php.sock'
-                    f.write('location %(url)s {\n'
-                            '    include %(fcgi_params_path)s;\n'
-                            '    fastcgi_param SCRIPT_FILENAME '
-                                    '%(version_folder)s$fastcgi_script_name;\n'
-                            '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
-                            '    fastcgi_param SCRIPT_NAME "";\n'
-                            '    fastcgi_pass unix:%(socket_path)s;\n'
-                            '}\n' % dict(entry,
-                                         socket_path=socket_path,
-                                         version_folder=version_folder,
-                                         fcgi_params_path=self.fcgi_params_path))
-
-                    share['programs'].append({
-                        'name': 'fcgi_php',
-                        'command': (
-                            '/usr/bin/spawn-fcgi -s %(socket_path)s -M 0777 '
-                            '-f /usr/bin/php5-cgi -n'
-                            % {'socket_path': socket_path})
-                    })
-
-                elif entry['type'] == 'fcgi':
-                    socket_uri = entry['socket']
-                    if socket_uri.startswith('tcp://'):
-                        socket = socket_uri[len('tcp://'):]
-                    elif socket_uri.startswith('unix:///'):
-                        socket = 'unix:' + socket_uri[len('unix://'):]
-                    else:
-                        raise ValueError("Can't parse socket %r" % socket_uri)
-                    f.write('location %(url)s {\n'
-                            '    include %(fcgi_params_path)s;\n'
-                            '    fastcgi_param PATH_INFO $fastcgi_script_name;\n'
-                            '    fastcgi_param SCRIPT_NAME "";\n'
-                            '    fastcgi_pass %(socket)s;\n'
-                            '}\n' % dict(entry,
-                                         socket=socket,
-                                         fcgi_params_path=self.fcgi_params_path))
-
-                else:
-                    raise NotImplementedError
-
+            f.write(conf_options)
+            f.write(conf_urlmap)
             f.write('}\n')
 
         ensure_folder(self.sites_folder)
