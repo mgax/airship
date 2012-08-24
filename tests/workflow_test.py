3531619eda387dd826f0dc796bea89f13b2094a5
Alex Morega <alex@grep.ro>
2012-08-24 12:55:54 +0300

    update workflow tests to use instance
    remove start/stop logic and the concept of versions

--- tests/workflow_test.py
+++ tests/workflow_test.py
@@ -1,96 +1,38 @@
 from path import path
 from mock import call
-from common import configure_deployment, imp
-from common import SargeTestCase
+from common import SargeTestCase, imp
 
 
 class WorkflowTest(SargeTestCase):
 
     def setUp(self):
         self.mock_supervisorctl = self.patch('sarge.daemons.Supervisor.ctl')
-        configure_deployment(self.tmp, {'name': 'testy'})
+        self.instance = self.sarge().new_instance()
 
-    def test_new_version(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-        self.assertTrue(version_folder.isdir())
-        self.assertEqual(version_folder.parent.parent, self.tmp)
+    def test_new_instance_creates_configuration_folder(self):
+        self.instance.start()
 
-    def test_versions_have_different_paths(self):
-        testy = self.sarge().get_deployment('testy')
-        version_path_1 = path(testy.new_version())
-        version_path_2 = path(testy.new_version())
-        self.assertNotEqual(version_path_1, version_path_2)
-
-    def test_activation_creates_configuration_folder(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-        testy.activate_version(version_folder)
-
-        cfg_folder = path(version_folder + '.cfg')
+        cfg_folder = path(self.instance.folder + '.cfg')
         self.assertTrue(cfg_folder.isdir())
 
-        symlink_path = self.tmp / imp('sarge.core').CFG_LINKS_FOLDER / 'testy'
+        symlink_path = (self.tmp /
+                        imp('sarge.core').CFG_LINKS_FOLDER /
+                        self.instance.id_)
         self.assertTrue(symlink_path.islink())
         self.assertEqual(symlink_path.readlink(), cfg_folder)
 
-    def test_activation_creates_runtime_folder(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-        testy.activate_version(version_folder)
+    def test_new_instance_creates_runtime_folder(self):
+        self.instance.start()
 
-        run_folder = path(version_folder + '.run')
+        run_folder = path(self.instance.folder + '.run')
         self.assertTrue(run_folder.isdir())
 
-    def test_activation_next_version_overwrites_symlink(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder_1 = path(testy.new_version())
-        testy.activate_version(version_folder_1)
-        version_folder_2 = path(testy.new_version())
-        testy.activate_version(version_folder_2)
-
-        cfg_folder_2 = path(version_folder_2 + '.cfg')
-        self.assertTrue(cfg_folder_2.isdir())
-
-        symlink_path = self.tmp / imp('sarge.core').CFG_LINKS_FOLDER / 'testy'
-        self.assertTrue(symlink_path.islink())
-        self.assertEqual(symlink_path.readlink(), cfg_folder_2)
-
-    def test_activation_triggers_supervisord_reread(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-
+    def test_instance_start_triggers_supervisord_reread_and_restart(self):
         self.mock_supervisorctl.reset_mock()
-        testy.activate_version(version_folder)
-        self.assertIn(call(['update']), self.mock_supervisorctl.mock_calls)
-
-    def test_activation_triggers_supervisord_restart_deployment(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-
-        self.mock_supervisorctl.reset_mock()
-        testy.activate_version(version_folder)
-        self.assertIn(call(['restart', 'testy:*']),
+        self.instance.start()
+        self.assertIn(call(['update']),
                       self.mock_supervisorctl.mock_calls)
-
-    def test_start_deployment_invokes_supervisorctl_start(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-        testy.activate_version(version_folder)
-
-        self.mock_supervisorctl.reset_mock()
-        testy.start()
-        self.assertIn(call(['start', 'testy:*']),
-                      self.mock_supervisorctl.mock_calls)
-
-    def test_stop_deployment_invokes_supervisorctl_stop(self):
-        testy = self.sarge().get_deployment('testy')
-        version_folder = path(testy.new_version())
-        testy.activate_version(version_folder)
-
-        self.mock_supervisorctl.reset_mock()
-        testy.stop()
-        self.assertIn(call(['stop', 'testy:*']),
+        self.assertIn(call(['restart', '%s:*' % self.instance.id_]),
                       self.mock_supervisorctl.mock_calls)
 
     def test_status_invokes_supervisorctl_status(self):
