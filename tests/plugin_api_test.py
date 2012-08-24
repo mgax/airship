d52a08ce576c476575b864df9919b2118a13663a
Alex Morega <alex@grep.ro>
2012-08-24 15:34:29 +0300

    rewrite plugin_api_test to use new instance api

--- tests/plugin_api_test.py
+++ tests/plugin_api_test.py
@@ -1,8 +1,7 @@
 import json
 from path import path
 from mock import Mock, call
-from common import configure_deployment, imp
-from common import SargeTestCase
+from common import SargeTestCase, imp
 
 
 mock_plugin = Mock()
@@ -20,18 +19,14 @@ class PluginApiTest(SargeTestCase):
         self.assertEqual(mock_plugin.mock_calls, [call(s)])
 
     def test_subscribe_to_activation_event(self):
-        configure_deployment(self.tmp, {'name': 'testy'})
-        s = self.sarge()
         mock_handler = Mock(im_self=None)
-        s.on_activate_version.connect(mock_handler)
-        testy = s.get_deployment('testy')
-        version_folder = testy.new_version()
-        testy.activate_version(version_folder)
+        sarge = self.sarge()
+        sarge.on_activate_version.connect(mock_handler)
+        sarge.new_instance().start()
         self.assertEqual(len(mock_handler.mock_calls), 1)
 
     def test_activation_event_passes_shared_dict(self):
-        configure_deployment(self.tmp, {'name': 'testy'})
-        s = self.sarge()
+        sarge = self.sarge()
 
         def handler1(depl, share, **extra):
             share['test-something'] = 123
@@ -41,27 +36,24 @@ class PluginApiTest(SargeTestCase):
         def handler2(depl, share, **extra):
             got.append(share.get('test-something'))
 
-        s.on_activate_version.connect(handler1)
-        s.on_activate_version.connect(handler2)
+        sarge.on_activate_version.connect(handler1)
+        sarge.on_activate_version.connect(handler2)
 
-        testy = s.get_deployment('testy')
-        version_folder = testy.new_version()
-        testy.activate_version(version_folder)
+        instance = sarge.new_instance()
+        instance.start()
         self.assertEqual(got, [123])
 
     def test_activation_event_allows_passing_info_to_application(self):
         def handler(depl, appcfg, **extra):
             appcfg['your-order'] = "is here"
 
-        configure_deployment(self.tmp, {'name': 'testy'})
-        s = self.sarge()
-        s.on_activate_version.connect(handler)
+        sarge = self.sarge()
+        sarge.on_activate_version.connect(handler)
 
-        testy = s.get_deployment('testy')
-        version_folder = testy.new_version()
-        cfg_folder = path(version_folder + '.cfg')
-        testy.activate_version(version_folder)
+        instance = sarge.new_instance()
+        instance.start()
 
+        cfg_folder = path(instance.folder + '.cfg')
         with (cfg_folder / imp('sarge.core').APP_CFG).open() as f:
             appcfg = json.load(f)
         self.assertEqual(appcfg['your-order'], "is here")
