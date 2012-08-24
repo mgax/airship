f408f640dbbe34fae1636abc5ca2f7b6e207d364
Alex Morega <alex@grep.ro>
2012-08-24 12:46:20 +0300

    update var_folder_test to use instance api

--- tests/var_folder_test.py
+++ tests/var_folder_test.py
@@ -1,7 +1,6 @@
 import json
 from path import path
-from common import configure_deployment, imp
-from common import SargeTestCase
+from common import SargeTestCase, imp
 
 
 class VarFolderTest(SargeTestCase):
@@ -11,26 +10,23 @@ class VarFolderTest(SargeTestCase):
                                    'plugins': ['sarge:VarFolderPlugin']})
 
     def configure_and_deploy(self):
-        configure_deployment(self.tmp, {
-            'name': 'testy',
-            'require-services': [
+        instance = self.sarge().new_instance({
+            'services': [
                 {'type': 'var-folder', 'name': 'db'},
             ],
         })
-        testy = self.sarge().get_deployment('testy')
-        version_folder = testy.new_version()
-        testy.activate_version(version_folder)
-        return version_folder
+        instance.start()
+        return instance
 
     def test_deploy_passes_var_folder_to_deployment(self):
-        version_folder = self.configure_and_deploy()
-        cfg_folder = path(version_folder + '.cfg')
+        instance = self.configure_and_deploy()
+        cfg_folder = path(instance.folder + '.cfg')
         with (cfg_folder / imp('sarge.core').APP_CFG).open() as f:
             appcfg = json.load(f)
-        db_path = self.tmp / 'var' / 'testy' / 'db'
+        db_path = self.tmp / 'var' / instance.id_ / 'db'
         self.assertEqual(appcfg['services']['db'], db_path)
 
     def test_deploy_creates_var_folder(self):
-        self.configure_and_deploy()
-        db_path = self.tmp / 'var' / 'testy' / 'db'
+        instance = self.configure_and_deploy()
+        db_path = self.tmp / 'var' / instance.id_ / 'db'
         self.assertTrue(db_path.isdir())
