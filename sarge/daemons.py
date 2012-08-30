141024324d7c727ce49724f29bfcf8cb807fd4ea
Alex Morega <alex@grep.ro>
2012-08-30 15:51:47 +0300

    'destroy' removes program from supervisor

--- sarge/daemons.py
+++ sarge/daemons.py
@@ -78,6 +78,7 @@ class Supervisor(object):
 
     def remove_instance(self, instance_id):
         self._instance_cfg(instance_id).unlink_p()
+        self.ctl(['update'])
 
     def ctl(self, cmd_args):
         base_args = [self.ctl_path, '-c', self.config_path]
