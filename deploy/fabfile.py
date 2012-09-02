3257f9382c5ecb889efd48130091fcf23cef7643
Alex Morega <alex@grep.ro>
2012-09-02 22:42:49 +0300

    deploy the wsgiref demo app

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,9 +1,27 @@
+from StringIO import StringIO
+import json
 from fabric.api import *
 from path import path
 
 
 env['sarge_home'] = '/var/local/sarge_demo'
 env['python_bin'] = 'python'
+
+
+DEMO_APP_TEMPLATE = """\
+#!/usr/bin/env python
+from wsgiref.simple_server import make_server, demo_app
+httpd = make_server("0.0.0.0", 8000, demo_app)
+httpd.serve_forever()
+"""
+
+
+def sarge(cmd):
+    return run("{sarge_home}/bin/sarge {cmd}".format(cmd=cmd, **env))
+
+
+def quote_json(config):
+    return "'" + json.dumps(config).replace("'", "\\u0027") + "'"
 
 
 @task
@@ -16,3 +34,28 @@ def bootstrap():
         run("curl -O 'https://raw.github.com/jaraco/path.py/2.3/path.py'")
         run("{python_bin} bootstrap-sarge.py".format(**env))
         run("bin/supervisord")
+
+
+@task
+def install(instance_id):
+    put(StringIO(DEMO_APP_TEMPLATE.format(**env)),
+        str(path(env['sarge_home']) / instance_id / 'server'),
+        mode=0755)
+
+
+@task
+def clean_old(name):
+    for instance in json.loads(sarge('list'))['instances']:
+        if instance['meta']['APPLICATION_NAME'] != name:
+            continue
+        sarge("stop " + instance['id'])
+        sarge("destroy " + instance['id'])
+
+
+@task
+def deploy(name='demo_app'):
+    execute('clean_old', name)
+    instance_config = {'application_name': name}
+    instance_id = sarge("new " + quote_json(instance_config))
+    execute('install', instance_id)
+    sarge("start '%s'" % instance_id)
