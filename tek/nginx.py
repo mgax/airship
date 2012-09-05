e28a2523271c7f773f0dbc34c2c7799512737c54
Alex Morega <alex@grep.ro>
2012-09-05 12:09:56 +0300

    reload nginx after configuring

--- tek/nginx.py
+++ tek/nginx.py
@@ -14,7 +14,11 @@ class NginxTek(object):
     def __init__(self, sites_dir):
         self.sites_dir = sites_dir
 
+    def reload_(self):
+        raise NotImplementedError
+
     def configure(self, site_name):
         port = 80
         cfg_path = self.sites_dir / "{site_name}:{port}".format(**locals())
         cfg_path.write_bytes(SITE_CFG.format(**locals()))
+        self.reload_()
--- tests/tek_test.py
+++ tests/tek_test.py
@@ -1,4 +1,5 @@
 import re
+from mock import call
 from common import HandyTestCase
 
 
@@ -8,9 +9,18 @@ def collapse(s):
 
 class TekNginxTest(HandyTestCase):
 
-    def test_nginx_configure_with_empty_urlmap_creates_blank_site(self):
+    def setUp(self):
+        self.reload_ = self.patch('tek.nginx.NginxTek.reload_')
+
+    def nginx_tek(self):
         from tek.nginx import NginxTek
-        nginx = NginxTek(sites_dir=self.tmp)
-        nginx.configure('zz.example.com')
+        return NginxTek(sites_dir=self.tmp)
+
+    def test_nginx_configure_with_empty_urlmap_creates_blank_site(self):
+        self.nginx_tek().configure('zz.example.com')
         self.assertEqual(collapse((self.tmp / 'zz.example.com:80').text()),
                          'server { server_name zz.example.com; listen 80; }')
+
+    def test_nginx_configure_triggers_nginx_reload(self):
+        self.nginx_tek().configure('zz.example.com')
+        self.assertEqual(self.reload_.mock_calls, [call()])
