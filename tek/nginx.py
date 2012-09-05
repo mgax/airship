f20dd76e77bdd1610139250add2585b880c40fe8
Alex Morega <alex@grep.ro>
2012-09-05 12:26:12 +0300

    allow non-default ports

--- tek/nginx.py
+++ tek/nginx.py
@@ -20,12 +20,11 @@ class NginxTek(object):
     def _cfg_path(self, site_name, port):
         return self.sites_dir / "{site_name}:{port}".format(**locals())
 
-    def configure(self, site_name):
-        port = 80
+    def configure(self, site_name, port=80):
         cfg_path = self._cfg_path(site_name, port)
         cfg_path.write_bytes(SITE_CFG.format(**locals()))
         self.reload_()
 
-    def delete(self, site_name):
-        self._cfg_path(site_name, 80).unlink()
+    def delete(self, site_name, port=80):
+        self._cfg_path(site_name, port).unlink()
         self.reload_()
--- tests/tek_test.py
+++ tests/tek_test.py
@@ -17,23 +17,23 @@ class TekNginxTest(HandyTestCase):
         return NginxTek(sites_dir=self.tmp)
 
     def test_nginx_configure_with_empty_urlmap_creates_blank_site(self):
-        self.nginx_tek().configure('zz.example.com')
-        self.assertEqual(collapse((self.tmp / 'zz.example.com:80').text()),
-                         'server { server_name zz.example.com; listen 80; }')
+        self.nginx_tek().configure('zz.example.com', 8080)
+        self.assertEqual(collapse((self.tmp / 'zz.example.com:8080').text()),
+                         'server { server_name zz.example.com; listen 8080; }')
 
     def test_nginx_configure_triggers_nginx_reload(self):
-        self.nginx_tek().configure('zz.example.com')
+        self.nginx_tek().configure('zz.example.com', 8080)
         self.assertEqual(self.reload_.mock_calls, [call()])
 
     def test_nginx_delete_removes_site(self):
         nginx = self.nginx_tek()
-        nginx.configure('zz.example.com')
-        nginx.delete('zz.example.com')
+        nginx.configure('zz.example.com', 8080)
+        nginx.delete('zz.example.com', 8080)
         self.assertEqual(self.tmp.listdir(), [])
 
     def test_nginx_delete_triggers_nginx_reload(self):
         nginx = self.nginx_tek()
-        nginx.configure('zz.example.com')
+        nginx.configure('zz.example.com', 8080)
         self.reload_.reset_mock()
-        nginx.delete('zz.example.com')
+        nginx.delete('zz.example.com', 8080)
         self.assertEqual(self.reload_.mock_calls, [call()])
