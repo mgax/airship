08e7fe5cb16cc343fa54eb4c79eed1e2896efe00
Alex Morega <alex@grep.ro>
2012-11-13 18:48:26 +0200

    use `virtualenv.py` from `dist` folder

--- sarge/deployer.py
+++ sarge/deployer.py
@@ -18,9 +18,11 @@ def deploy(sarge, tarfile, procname):
         index_dir = sarge.config['wheel_index_dir']
         venv = bucket.folder / '_virtualenv'
         pip = venv / 'bin' / 'pip'
+        virtualenv_py = sarge.home_path / 'dist' / 'virtualenv.py'
         python = sarge.config.get('virtualenv_python_bin', 'python')
 
-        subprocess.check_call(['virtualenv', venv, '--python=' + python,
+        subprocess.check_call(['python', virtualenv_py, venv,
+                               '--python=' + python,
                                '--distribute', '--never-download',
                                '--extra-search-dir=' + index_dir])
         subprocess.check_call([pip, 'install', 'wheel', '--no-index',
