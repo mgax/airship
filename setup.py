7a767c40d840c1f3af95a4d0a8e2b043b2edd153
Alex Morega <alex@grep.ro>
2013-02-28 16:17:59 +0200

    enumerate the `airship.contrib` package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 7):
 distutils.core.setup(
     name='Airship',
     version='0.3-dev',
-    packages=['airship', 'airship.contrib.python'],
+    packages=['airship', 'airship.contrib', 'airship.contrib.python'],
     install_requires=dependencies,
     entry_points={
         'console_scripts': ['airship = airship.core:main'],
